2da92f7fab5d0af322d03234343dbf1d5f9df646
kevinkl3 <kevinlopez@unitec.edu>
2013-08-19 18:00:35 -0600

    sdsd

--- src/com/duobitsw/easybuy/MainActivity.java
+++ src/com/duobitsw/easybuy/MainActivity.java
@@ -2,7 +2,11 @@ package com.duobitsw.easybuy;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button login_btn = (Button) findViewById(R.id.button1);
+		
+		login_btn.setOnClickListener(new OnClickListener(){
+			public void onClick(View v) {
+				Intent i = new Intent(getApplicationContext(),LoginActivity.class);
+				startActivity(i);
+			}
+			
+		});
 	}
 
 	@Override
