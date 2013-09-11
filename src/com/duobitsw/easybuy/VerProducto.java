f12a63c709580457d7fd78e5e15a87c04247c65b
kevinkl3 <kevinlopez@unitec.edu>
2013-09-11 02:12:19 -0600

    agregado ver producto

--- src/com/duobitsw/easybuy/VerProducto.java
+++ src/com/duobitsw/easybuy/VerProducto.java
@@ -2,14 +2,18 @@ package com.duobitsw.easybuy;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class VerProducto extends Activity {
 	int PRODUCTO_ID;
 	
 	String Datos[][] = {
-			{"CocaCola","Lps. 17.99"},
-			{"Judo de Naranja Sula","Lps. 14.57"}
+			{"Judo de Naranja Sula","Lps. 14.57",""+R.drawable.prod_0},
+			{"CocaCola","Lps. 17.99",""+R.drawable.prod_1},
+			{"MacBook Pro","$ 500",""+R.drawable.prod_2}
 	};
 	
 	
@@ -24,7 +28,16 @@ public class VerProducto extends Activity {
 	}
 
 	private void setDatos(){
+		if(PRODUCTO_ID >= Datos.length)return;
 		
+		Drawable d = getResources().getDrawable(Integer.parseInt(Datos[PRODUCTO_ID][2]) )
+				;
+		TextView nombre = (TextView) findViewById(R.id.producto_nombre);
+		TextView precio = (TextView) findViewById(R.id.precio_val);
+		ImageView imagen = (ImageView) findViewById(R.id.prod_img);
+		nombre.setText(Datos[PRODUCTO_ID][0]);
+		precio.setText(Datos[PRODUCTO_ID][1]);
+		imagen.setImageDrawable(d);
 	}
 	
 	@Override
